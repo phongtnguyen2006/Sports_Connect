3349b60b72f25df176902333bf24ff94235b851b
Joji Morikawa <morikawaj@g.ucla.edu>
2026-05-28 12:55:28 -0700

    new files EditProfile.jsx and UserSettings.jsx

---
+++ frontend/src/pages/EditProfile.jsx
@@ -0,0 +1,65 @@
+
+import {useState,useRef} from "react";
+function ProfilePage(){
+    const [imageUrl,setImageUrl] = useState("/image/images-2.jpeg");
+    const fileInputRef = useRef(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+
+        if(file) {
+            const previewUrl = URL.createObjectURL(file);
+            setImageUrl(previewUrl);
+        }
+    }
+};
+function EditProfile(){
+
+
+
+    return(
+        <div className="edit-profile-page">
+            <section className="profile-picture"> 
+                
+                <img
+                    className="profile-image"
+                    src="/images/images-2.jpeg"
+                    alt="John Doe profile"
+                    onClick={()=> fileInputRef.current.click()}
+                />
+                <div className="temp-make-button-edit-profile-photo">
+                    Edit Picture
+                </div>
+
+            </section>
+            <section className="profile-name">
+                <div>
+                    Name
+                    <input type="text" id="Name" placeholder="John Doe"/>
+                   
+                </div>
+
+            </section>
+            <section className="username">
+                <div>
+                    User name
+                    <input type="text" id="username" placeholder="@John Doe"/>
+                </div>
+
+            </section>
+            <section className="Bio">
+                <div>
+                    Bio
+                    <input type="text" id="username" placeholder="Enter Bio"/>
+                    
+                </div>
+
+            </section>
+
+        </div>
+
+    );
+
+
+}
+export default EditProfile;
