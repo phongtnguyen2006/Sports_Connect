8db2d2475d91ec0a8e218588182e52744e60ed09
eyemkey <amkrtchyan1101@gmail.com>
2026-06-01 16:13:52 -0700

    Fixed event.js typedef to match current events table

--- backend/src/models/event.js
+++ backend/src/models/event.js
@@ -1,14 +1,15 @@
 /**
  * @typedef {Object} Event
  * @property {string} id
+ * @property {string} host_id
  * @property {string} title
- * @property {string} description
- * @property {string} date - ISO date (YYYY-MM-DD)
- * @property {string} time - HH:mm
- * @property {string} location
- * @property {string} sport
- * @property {string} hostUsername
- * @property {string} createdAt - ISO timestamp
+ * @property {string|null} description
+ * @property {string} starts_at
+ * @property {string|null} ends_at
+ * @property {string|null} location
+ * @property {string|null} sport
+ * @property {number|null} max_attendees
+ * @property {string} created_at - ISO timestamp
  */
 
 export {};
